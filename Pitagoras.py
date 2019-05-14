71472d3b7f76753e0369273761867f4a04dbb7e8
JosephBravo <39282996+JosephBravo@users.noreply.github.com>
2019-05-13 22:05:20 -0500

    Update Pitagoras.py

--- Pitagoras.py
+++ Pitagoras.py
@@ -5,7 +5,7 @@ print ("Cual es el producto de abc")
 for a in range(1, 1000):  #Empezamos la secuencia donde a esta entre 1 - 1000
     for b in range(a, 1000):  # sabemos que b es mayor que a (a<b<c)
         c = 1000 - a - b    #Restamos a y b a 1000 para encontrar c
-        if c > 0:
+        if c > 5: # sabemos que c es mayor que a (a<b<c)
             if a ** 2 + b ** 2 == c ** 2:   #Validamos que cumpla el teorema de pitagoras
                 pitagoras = a*b*c
 
