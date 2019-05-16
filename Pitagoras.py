37d7abdf8ec4a3116f399919c35727f24b64c46f
JosephBravo <39282996+JosephBravo@users.noreply.github.com>
2019-05-16 09:16:29 -0500

    Update Pitagoras.py

--- Pitagoras.py
+++ Pitagoras.py
@@ -1,13 +1,18 @@
-
 print ("Existe solo una tripleta de Pitagoras para la cual a + b + c = 1000")
 print ("Cual es el producto de abc")
 
-for a in range(1, 1000):  #Empezamos la secuencia donde a esta entre 1 - 1000
-    for b in range(a, 1000):  # sabemos que b es mayor que a (a<b<c)
-        c = 1000 - a - b    #Restamos a y b a 1000 para encontrar c
-        if c > 5: # sabemos que c es mayor que a (a<b<c)
-            if a ** 2 + b ** 2 == c ** 2:   #Validamos que cumpla el teorema de pitagoras
-                pitagoras = a*b*c
 
-                print ("a es:",a, "  b es:",b, "  c es:",c, "a+b+c=1000")
-                print ("El producto de abc es:", pitagoras)
+def Pitagoras(a, b, c):
+    if not (a < b and b < c):
+        return False
+    return a ** 2 + b ** 2 == c ** 2
+
+universo = [x for x in range(1, 1000)]
+
+for a in universo:
+    for b in range(a, 1000 - a):
+        for c in range(b, 1000 - b):
+            if a + b + c == 1000:
+                if Pitagoras(a, b, c):
+                    print ("a es:",a, "  b es:",b, "  c es:",c, "a+b+c=1000")
+                    print("El producto de abc es:",(a * b * c))
